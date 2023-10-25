50ff209ed5ffd796b9d9614ccf2c512ee899cd87
Eric Todd <roboevt@gmail.com>
2023-10-25 17:14:04 -0500

    Added scenario to base_sir and published

--- part2.m
+++ part2.m
@@ -27,3 +27,21 @@ plot(omicron_dates, omicron_cases)
 title("Omicron Cases")
 xlabel("Date")
 ylabel("Cases")
+
+% SIRD Model
+
+deltaA = [0.95 0.04 0.01 0; 
+    0.05 0.85 0.01 0; 
+    0 0.1 0.98 0; 
+    0 0.01 0 1];
+
+delta0 = [1 0 0 0];
+
+sys_sir_base = ss(deltaA,B,eye(4),zeros(4,1),1);
+Y = lsim(sys_sir_base,zeros(1000,1),linspace(0,999,1000),delta0);
+
+figure
+plot(Y)
+legend('S','I','R','D');
+xlabel('Time')
+ylabel('Percentage Population');
